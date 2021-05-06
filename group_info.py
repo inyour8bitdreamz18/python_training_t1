2471384248e90096c2486347231ebccab5f2aa87
inyour8bitdreamz18 <WaginElla2019>
2021-05-06 15:20:27 +0300

    refactored code, added new class GroupInfo, introduced parameters

---
+++ group_info.py
@@ -0,0 +1,6 @@
+#class contained some info group
+class GroupInfo:
+    def __init__(self, Name, Header, Footer):
+        self.Name = Name
+        self.Header = Header
+        self.Footer = Footer
