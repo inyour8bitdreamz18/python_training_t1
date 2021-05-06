e89be7874754369aa6d71fd7e14554048477fa6f
inyour8bitdreamz18 <WaginElla2019>
2021-05-06 16:19:52 +0300

    added class ContactInfo

---
+++ contact_info.py
@@ -0,0 +1,27 @@
+#class ContactInfo contained contact info
+
+class ContactInfo:
+    def __init__(self, firstname, middlename, lastname, nickname, title, company, address, mobile_number,
+                        home_number, work, fax, email, bday, bmonth, byear, aday, amonth, ayear, address2, notes,
+                        phone2):
+        self.firstname = firstname
+        self.middlename = middlename
+        self.lastname = lastname
+        self.nickname = nickname
+        self.title = title
+        self.company = company
+        self.address = address
+        self.mobile_number = mobile_number
+        self.home_number = home_number
+        self.work = work
+        self.fax = fax
+        self.email = email
+        self.bday = bday
+        self.bmonth = bmonth
+        self.byear = byear
+        self.aday = aday
+        self.amonth = amonth
+        self.ayear = ayear
+        self.address2 = address2
+        self.notes = notes
+        self.phone2 = phone2
