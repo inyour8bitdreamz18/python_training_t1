58507e06b3df6aa77f088a2bbe3b47e3915b9dd4
inyour8bitdreamz18 <WaginElla2019>
2021-05-24 15:42:42 +0300

    added login() and logout() in test_modify_group

--- test/test_modify_group.py
+++ test/test_modify_group.py
@@ -1,6 +1,10 @@
 from model.group_info import GroupInfo
 def test_modify_group_name(app):
+    app.session.login(username="admin", password="secret")
     app.group.modify_first_group(GroupInfo(name="New group"))
+    app.session.logout()
 
 def test_modify_group_header(app):
+    app.session.login(username="admin", password="secret")
     app.group.modify_first_group(GroupInfo(header="New header"))
+    app.session.logout()
